fa0e4fea01e95808b9df0048a274cd2cf74ad124
vietnt <vietnt@bfast.dev>
2024-12-18 22:41:54 +0700

    Initial Commit

---
+++ swiftui_fade_scroll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui_fade_scroll
+//
+//  Created by Viet Nguyen Tran on 18/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui_fade_scroll/swiftui_fade_scrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_fade_scrollApp.swift
+//  swiftui_fade_scroll
+//
+//  Created by Viet Nguyen Tran on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_fade_scrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
